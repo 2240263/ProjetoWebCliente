2bebca1e72336058662214ec2f9a9d350763098b
2240263 <2240263@my.ipleiria.pt>
2024-12-21 16:57:25 +0000

    Homepage (praticamente concluida)
    Ajustes no fim do projecto

--- assets/js/JavaScript.js
+++ assets/js/JavaScript.js
@@ -1,3 +1,5 @@
+//pesquisa de países
+
 $(document).ready(function() {
     $("#pesquisarbotao").on("click", function() {
         var inputValue = $("#pesquisartext").val();
@@ -44,4 +46,49 @@ function displayPaises(arrayPaises) {
         paisList.append(cardPais);
     });
 }
+
+
+//HOMEPAGE
+
+$(document).ready(function () {
+    // Função para carregar países aleatórios
+    function carregarPaises() {
+      // URL da API para carregar os países
+      $.ajax({
+        method: 'GET',
+        url: 'https://restcountries.com/v3.1/all', // API para obter os países
+        success: function (dados) {
+          // Embaralha o array de países e seleciona 3 países aleatórios
+          var paisesAleatorios = shuffleArray(dados).slice(0, 3);
+  
+          // Preenche os dados dos três primeiros países nos cards existentes
+          $('#f1').attr('src', paisesAleatorios[0].flags.png); // Bandeira do Card 1
+          $('#title1').text(paisesAleatorios[0].name.common); // Nome do Card 1
+  
+          $('#f2').attr('src', paisesAleatorios[1].flags.png); // Bandeira do Card 2
+          $('#title2').text(paisesAleatorios[1].name.common); // Nome do Card 2
+  
+          $('#f3').attr('src', paisesAleatorios[2].flags.png); // Bandeira do Card 3
+          $('#title3').text(paisesAleatorios[2].name.common); // Nome do Card 3
+        },
+        error: function () {
+          alert('Erro ao carregar os países.');
+        }
+      });
+    }
+  
+    // Função para embaralhar o array de países
+    function shuffleArray(array) {
+      var shuffledArray = array.slice();
+      for (var i = shuffledArray.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      }
+      return shuffledArray;
+    }
+  
+    // Chama a função para carregar os países aleatórios ao carregar a página
+    carregarPaises();
+  });
+  
 
